67502055025315164cef2ce600460091e529c597
Caio Portella <caiohportella@gmail.com>
2024-03-22 22:36:11 -0300

    responsiveness ajustments

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -22,7 +22,7 @@ const Skill = ({ directionLeft, skill }: Props) => {
         width={100}
         height={100}
       />
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-16 h-16 md:w-20 md:h-20 lg:w-20 lg:h-20 xl:w-20 xl:h-20 2xl:w-28 2xl:h-28 rounded-full z-0">
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 lg:w-10 lg:h-10 xl:w-20 xl:h-20 2xl:w-28 2xl:h-28 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
           <p className="md:text-3xl font-bold text-black opacity-100">
             {skill?.progress}%
